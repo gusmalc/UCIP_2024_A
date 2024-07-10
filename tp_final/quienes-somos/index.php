ab6cbe289cb90b22a00987fe6a65d06e3f00aed6
Gustavo Malcotti <gustavo@malcotti.com>
2024-07-10 19:00:14 -0300

    1

--- tp_final/quienes-somos/index.php
+++ tp_final/quienes-somos/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manzanita Feliz - Quienes Somos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo $site_url . "asest/css/estilos.css"; ?>">
+    <link rel="stylesheet" href="<?php echo $site_url . "assets/css/estilos.css"; ?>">
 </head>
 <body>
     <?php include '../common/menu.php'; ?>
